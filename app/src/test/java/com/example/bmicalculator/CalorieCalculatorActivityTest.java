d9baf7cf2707b160ac4acc8b3cd094f9cc53bf73
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-04-25 21:51:01 +0200

    zadanie3

--- app/src/test/java/com/example/bmicalculator/CalorieCalculatorActivityTest.java
+++ app/src/test/java/com/example/bmicalculator/CalorieCalculatorActivityTest.java
@@ -10,13 +10,13 @@ public class CalorieCalculatorActivityTest {
         double result = CalorieCalculatorActivity.calculateCalories(70, 175, 25, true, 1.55);
 
         //1735.62 * 1.55 = 2690.21
-        assertEquals(2690, result, 1); // tolerancja 1 kcal
+        assertEquals(2690, result, 1);
     }
 
     @Test
     public void testCalculateCaloriesForFemale() {
         double result = CalorieCalculatorActivity.calculateCalories(60, 165, 30, false, 1.375);
         //1393.85 * 1.375 = 1916.54
-        assertEquals(1916, result, 100); // tolerancja 1 kcal
+        assertEquals(1916, result, 100);
     }
 }
